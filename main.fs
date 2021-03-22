301628e835f5ee296f1b775df877e7d14f5f91b6
RichieVerdi75 <78509873+RichieVerdi75@users.noreply.github.com>
2021-03-22 19:11:11 -0400

    Update main.fs

--- main.fs
+++ main.fs
@@ -5,10 +5,24 @@ type Tree =
     | Leaf of int
 
 let rec prod (t:Tree) :int =
-    0
-
+    let (l,f) match t with
+    | Leaf a -> a // matching the Leaf subtype
+    | Node (l, r) -> l * r // or matching the Node subtype
+    | _ -> 0
+    
 let rec map (f:int->int) (t:Tree) :Tree =
-    t
+    match t with
+    | Leaf a -> a
+    | Node (l, r) -> l * r
+    | _ -> t
 
 let rec foldStr (nf:string -> string -> string) (lf:int->string) (t:Tree) :string =
-    ""
+    match t with
+    | Leaf a -> a
+    | Node (l, r) -> l * r
+    | _ -> ""
+
+// let result (r:Error) :int =
+//     match r with
+//       OK -> 0
+//       Err n -> n
